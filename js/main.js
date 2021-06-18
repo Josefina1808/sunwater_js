e915e9893fd41c6ca75cd17a26b377992211a3d9
Josefina Esnaola Borbore <josefinaesna@gmail.com>
2021-06-18 20:53:19 -0300

    JS con objetos

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 /* 
 FUNCIONES A REALIZAR
-Agregar productos a lista de me gusta
-Agregar productos al carrrito
+X Agregar productos a lista de me gusta 
+X Agregar productos al carrrito
 Filtrado de producto por: categoría, palabras
 Ordenar por: menor precio, mayor precio, 
-Calcular subtotal sin Envío X
-Calculador envío según zona X
-Calcular Total de la compra x
-Calcular IVA de la compra (los precios ya tienen IVA) X
+X Calcular subtotal sin Envío 
+X Calculador envío según zona 
+X Calcular Total de la compra 
+X Calcular IVA de la compra (los precios ya tienen IVA) 
 PASOS EN LA COMPPRA
 1° Agrega al carrito
 2° Finalizar compra
@@ -15,16 +15,7 @@ PASOS EN LA COMPPRA
 5° Calcular envío (Sumar al total compra)
 4° Pago
 5° Confirmación*/
-
-let cart = [];
-let like = [];
-// Necesito un condicional para agregar los productos a las lista, es decir, si like === true => lo agrega
-
-//FILTRADO: dependiendo de lo seleccionado, es lo que se muestra
-
-//Ordenar por...: debo comparar precio => ordernar de mayor a menor || de menor a mayor
-
-function sumar() {
+function sumar(lista) {
     let resultado = 0;
     for (let i = 0; i < lista.length; i++) {
         resultado += lista[i];
@@ -32,31 +23,9 @@ function sumar() {
     return resultado;
 }
 
-let subtotal = sumar(cart.product.price);
-
-let envio = function envio() {
-    switch (key) { //según lo que seleccione el usuario en el form es el tipo de caso
-        case 'Interior':
-            envio = 1000;
-            break;
-        case 'CABA':
-            envio = 250;
-            break;
-        case 'Buenos Aires':
-            envio = 600;
-            break;
-        case 'PickUp':
-            envio = 0;
-            break;
-        default:
-            alert('No seleccionó forma de envío')
-            break;
-    }
-}
-
-let total = subtotal + envio;
-
-let iva = (total) => total * 0.21;
+let cart = [];
+let like = [];
+let products = [];
 
 class product {
     constructor(id, titule, price, stock, category) {
@@ -76,13 +45,69 @@ class product {
             amount: this.getSubtotal(qty),
         }
     }
-    addToCart = function () {
-        cart.push(this.getBuy())
+
+    addToCart = function (qty) {
+        cart.push(this.getBuy(qty))
+    }
+
+    addToLike = function () {
+        like.push({
+            product: this.titule,
+            price: this.price,
+        })
+    }
+
+    adToProducts = function () {
+        return products.push({
+            id: this.id,
+            titule: this.titule,
+            price: this.price,
+            stock: this.stock,
+            category: this.category,
+        })
+    }
+
+    filtar = function (key) {
+        
+    }
+
+}
+
+const BOTELLA1 = new product(001, 'Botella Flor de la Vida', 1000, 100, 'Botellas')
+const BOTELLA2 = new product(002, 'Botella Metatrón', 1000, 70, 'Botellas')
+const POSA_BOTELLA = new product(003, 'Posa Botella', 469, 50, 'Posa Botellas')
+const PULSERA_CELESTE = new product(004, 'Pulsera Metratón', 439, 20, 'Pulseras')
+const PULSERA_AZUL = new product(005, 'Pulsera Flor de la Vida', 439, 20, 'Pulseras')
+
+BOTELLA1.adToProducts()
+BOTELLA2.adToProducts()
+POSA_BOTELLA.adToProducts()
+PULSERA_CELESTE.adToProducts()
+PULSERA_AZUL.adToProducts()
+console.log(products)
+
+let subtotal = sumar(cart.map(e => e.amount));
+
+
+let envio = function calculadorEnvio(key) {
+    switch (key) { //según lo que seleccione el usuario en el form es el tipo de caso
+        case 'Interior':
+            return 400;
+
+        case 'CABA':
+            return 150;
+
+        case 'Buenos Aires':
+            return 300;
+
+        case 'PickUp':
+            return 0;
+
+        default:
+            return alert('No seleccionó forma de envío')
     }
 }
 
-const BOTELLA = new product(001, 'Botella Azul Para Solarizar', 1000, 100, 'Botellas')
-const POSA_BOTELLA = new product(002, 'Posa Botella', 469, 50, 'Posa Botellas')
-const PULSERA_CELESTE = new product(003, 'Pulsera Metratón', 439, 20, 'Pulseras')
-const PULSERA_AZUL = new product(004, 'Pulsera Flor de la Vida', 439, 20, 'Pulseras')
+let total = subtotal + envio;
 
+let iva = (total) => total * 0.21;
