dd1f13b12bf26c1a6f79fcaab67f1852346dd1cf
Josefina Esnaola Borbore <josefinaesna@gmail.com>
2021-06-26 20:50:00 -0300

    Utilizando innnerHtml

--- js/main.js
+++ js/main.js
@@ -28,12 +28,14 @@ let like = [];
 let products = [];
 
 class product {
-    constructor(id, titule, price, stock, category) {
+    constructor(id, titule, price, stock, category, description, photo) {
         this.id = id
         this.titule = titule
         this.price = price
         this.stock = stock
         this.category = category
+        this.description = description
+        this.photo = photo
     }
     getSubtotal = function (qty) {
         return this.price * qty
@@ -41,6 +43,7 @@ class product {
     getBuy = function (qty) {
         return {
             product: this.titule,
+            product: this.id,
             quantity: qty,
             amount: this.getSubtotal(qty),
         }
@@ -50,6 +53,10 @@ class product {
         cart.push(this.getBuy(qty))
     }
 
+    deleteProduct = (id) => {
+        cart.filter(product => this.id !== id) //no se si funcione
+    }
+
     addToLike = function () {
         like.push({
             product: this.titule,
@@ -57,13 +64,14 @@ class product {
         })
     }
 
-    adToProducts = function () {
+    addToProducts = function () {
         return products.push({
             id: this.id,
             titule: this.titule,
             price: this.price,
             stock: this.stock,
             category: this.category,
+            description: this.description
         })
     }
 
@@ -71,20 +79,49 @@ class product {
         
     }
 
+
+
 }
 
-const BOTELLA1 = new product(001, 'Botella Flor de la Vida', 1000, 100, 'Botellas')
-const BOTELLA2 = new product(002, 'Botella Metatrón', 1000, 70, 'Botellas')
-const POSA_BOTELLA = new product(003, 'Posa Botella', 469, 50, 'Posa Botellas')
-const PULSERA_CELESTE = new product(004, 'Pulsera Metratón', 439, 20, 'Pulseras')
-const PULSERA_AZUL = new product(005, 'Pulsera Flor de la Vida', 439, 20, 'Pulseras')
+const BOTELLA1 = new product(001, 'Botella Flor de la Vida', 1000, 100, 'Botellas', 'Botella para solarizar')
+const BOTELLA2 = new product(002, 'Botella Metatrón', 1000, 70, 'Botellas', 'Botella para solarizar')
+const POSA_BOTELLA_flor = new product(003, 'Posa Botella', 469, 50, 'Posa Botellas', 'Para solarización')
+const POSA_BOTELLA_metatron = new product(003, 'Posa Botella', 469, 50, 'Posa Botellas', 'Para solarización')
+const PULSERA_CELESTE = new product(004, 'Pulsera Metratón', 439, 20, 'Pulseras', 'Pulsera con energía positiva' )
+const PULSERA_AZUL = new product(005, 'Pulsera Flor de la Vida', 439, 20, 'Pulseras', 'Pulsera con energía positiva')
 
-BOTELLA1.adToProducts()
-BOTELLA2.adToProducts()
-POSA_BOTELLA.adToProducts()
-PULSERA_CELESTE.adToProducts()
-PULSERA_AZUL.adToProducts()
+BOTELLA1.addToProducts()
+BOTELLA2.addToProducts()
+POSA_BOTELLA_flor.addToProducts()
+POSA_BOTELLA_metatron.addToProducts()
+PULSERA_CELESTE.addToProducts()
+PULSERA_AZUL.addToProducts()
 console.log(products)
+
+const productsDiv = document.querySelector('body main section div')
+productsDiv.innerHTML = ''
+
+let html = '';
+products.forEach( product => {
+    html += `
+    <div class="card__product producto-${product.id}"> 
+                    <div class="card_product--titule_principal">
+                        <div class="card__product--titule">${product.titule}</div>
+                        <div class="card__product--desc">${product.description}r</div>
+                    </div> 
+                    <div class="card__product--img">
+                        <img src="../img/9.jpg" alt="">
+                    </div>
+                    <div class="card__product--cta">
+                        <div class="price">$${product.price}</div>
+                        <button class="btn addToCart">AGREGAR</button>
+                    </div>
+     </div> 
+    `
+})
+
+productsDiv.innerHTML = html;
+
 
 let subtotal = sumar(cart.map(e => e.amount));
 
