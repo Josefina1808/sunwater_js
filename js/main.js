6f61ce465ddefac033fe65803db1abeb9f770065
Josefina Esnaola Borbore <josefinaesna@gmail.com>
2021-07-03 19:43:25 -0300

    Agregando el carrito

--- js/main.js
+++ js/main.js
@@ -76,20 +76,14 @@ class product {
         })
     }
 
-    filtar = function (key) {
-        
-    }
-
-
-
 }
 
 const BOTELLA1 = new product(001, 'Botella Flor de la Vida', 1000, 100, 'Botellas', 'Botella para solarizar')
 const BOTELLA2 = new product(002, 'Botella Metatrón', 1000, 70, 'Botellas', 'Botella para solarizar')
 const POSA_BOTELLA_flor = new product(003, 'Posa Botella', 469, 50, 'Posa Botellas', 'Para solarización')
-const POSA_BOTELLA_metatron = new product(003, 'Posa Botella', 469, 50, 'Posa Botellas', 'Para solarización')
-const PULSERA_CELESTE = new product(004, 'Pulsera Metratón', 439, 20, 'Pulseras', 'Pulsera con energía positiva' )
-const PULSERA_AZUL = new product(005, 'Pulsera Flor de la Vida', 439, 20, 'Pulseras', 'Pulsera con energía positiva')
+const POSA_BOTELLA_metatron = new product(004, 'Posa Botella', 469, 50, 'Posa Botellas', 'Para solarización')
+const PULSERA_CELESTE = new product(005, 'Pulsera Metratón', 439, 20, 'Pulseras', 'Pulsera con energía positiva' )
+const PULSERA_AZUL = new product(006, 'Pulsera Flor de la Vida', 439, 20, 'Pulseras', 'Pulsera con energía positiva')
 
 BOTELLA1.addToProducts()
 BOTELLA2.addToProducts()
@@ -97,31 +91,6 @@ POSA_BOTELLA_flor.addToProducts()
 POSA_BOTELLA_metatron.addToProducts()
 PULSERA_CELESTE.addToProducts()
 PULSERA_AZUL.addToProducts()
-console.log(products)
-
-const productsDiv = document.querySelector('body main section div')
-productsDiv.innerHTML = ''
-
-let html = '';
-products.forEach( product => {
-    html += `
-    <div class="card__product producto-${product.id}"> 
-                    <div class="card_product--titule_principal">
-                        <div class="card__product--titule">${product.titule}</div>
-                        <div class="card__product--desc">${product.description}r</div>
-                    </div> 
-                    <div class="card__product--img">
-                        <img src="../img/9.jpg" alt="">
-                    </div>
-                    <div class="card__product--cta">
-                        <div class="price">$${product.price}</div>
-                        <button class="btn addToCart">AGREGAR</button>
-                    </div>
-     </div> 
-    `
-})
-
-productsDiv.innerHTML = html;
 
 
 let subtotal = sumar(cart.map(e => e.amount));
@@ -149,3 +118,98 @@ let envio = function calculadorEnvio(key) {
 let total = subtotal + envio;
 
 let iva = (total) => total * 0.21;
+
+const renderProducts = array => {
+    const productsSection = document.querySelector('body main section div.productos')
+    //si el parámetro es false o está vaío
+    if (!array || array.length === 0) {
+        productsSection.innerHTML = '<p>No hay productos :(</p>';
+        return;
+    }
+    productsSection.innerHTML = '';
+    let html = '';
+
+    array.forEach(product => {
+        html += `
+        <div class="card__product producto-${product.id}"> 
+                        <div class="card_product--titule_principal">
+                            <div class="card__product--titule">${product.titule}</div>
+                            <div class="card__product--desc">${product.description}r</div>
+                        </div> 
+                        <div class="card__product--img">
+                            <img src="../img/products/${product.id}.jpg" alt="">
+                        </div>
+                        <div class="card__product--cta">
+                            <div class="price">$${product.price}</div>
+                            <button class="btn btn-buy" value="${product.id}">AGREGAR</button>
+                        </div>
+         </div> 
+        `;
+    });
+
+    productsSection.innerHTML = html;
+}
+
+const addToCart = evento => {
+   
+    const idDelProductoABuscar = evento.target.value;
+
+    const buscarProductoEnDB = products.find(elemento => elemento.id === idDelProductoABuscar);
+    
+    cart.push(buscarProductoEnDB);
+    //Acá termina la lógica para agregar al ARRAY del cart 
+    ///////////////////////////////////////////////////////////
+    //Aquí va lo lógica para mostrar en el HTML
+    renderCart(cart)
+}
+
+const renderCart = array => {
+    const cartSection = document.querySelector('body main section div.cart')
+    //si el parámetro es false o está vacio
+    if (!array || array.length === 0) {
+        cartSection.innerHTML = '<p>Tu carrito está vacío</p>';
+        return;
+    }
+    cartSection.innerHTML = '';
+    let html = '';
+
+    array.forEach(product => {
+        
+        html += `
+        <div class="card__product producto-${product.id}"> 
+                        <div class="card_product--titule_principal">
+                            <div class="card__product--titule">${product.titule}</div>
+                            <div class="card__product--desc">${product.description}r</div>
+                        </div> 
+                        <div class="card__product--img">
+                            <img src="../img/products/${product.id}.jpg" alt="">
+                        </div>
+                        <div class="card__product--cta">
+                            <div class="price">$${product.price}</div>
+                            <button class="btn btn-delete" onclick="deleteFromCart(${product.id})">X</button>
+                        </div>
+         </div> 
+         `;
+    });
+
+    cartSection.innerHTML = html;
+}
+
+const deleteFromCart = id => {
+    const buscarProductoEnCarritoYBorrar = cart.filter(producto => producto.id !== id);
+    cart = buscarProductoEnCarritoYBorrar;
+    //Aquí termina la lógica de eliminar 
+    ////////////
+    //Ahora hay que renderizar
+    renderCart(cart);
+}
+
+
+window.onload = () => { 
+   renderProducts(products);
+   renderCart(cart);
+   const btnBuy = document.querySelectorAll('.btn-buy');
+   const btnDelete = document.querySelectorAll('.btn-delete');
+
+   btnBuy.forEach(btn => btn.addEventListener('click', addToCart));
+}
